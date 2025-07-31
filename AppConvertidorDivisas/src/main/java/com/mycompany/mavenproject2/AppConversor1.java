a44ca0c8a1ff2b887e93d727ff5a60ae0c8ff883
folkiens <157939480+folkiens@users.noreply.github.com>
2025-07-31 00:45:14 -0500

    Update AppConversor1.java

--- AppConvertidorDivisas/src/main/java/com/mycompany/mavenproject2/AppConversor1.java
+++ AppConvertidorDivisas/src/main/java/com/mycompany/mavenproject2/AppConversor1.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.mavenproject2;
 
-/**
- *
- * @author JoshV
- */
 public class AppConversor1 {
 
     public static void main(String[] args) {
